bd6d12e7b2b54186ecb4d3a702860463bcbbc6a3
Anant Kumar Mann <37663597+anantmann9057@users.noreply.github.com>
2024-12-10 18:39:55 +0530

    Add files via upload
    initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from 'axios';
+
+const app = express();
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+const port = 80;
+
+app.get('/', (req, res) => {
+  //'https://dummyjson.com/products?limit=10&skip=10&select=title,price'
+
+
+
+  axios.get('https://dummyjson.com/recipes')
+    .then(function (response) {
+      res.render('index.ejs', {
+        recipes: response.data,
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+    .finally(function () {
+    });
+
+});
+//'
+app.get('/post/:id', (req, res) => {
+  console.log(req.params);
+
+  axios.get(`https://dummyjson.com/recipes/1${req.params['id']}`)
+    .then(function (response) {
+      console.log(response.data);
+      res.render('post.ejs', {
+        recipes: response.data
+      })
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+    .finally(function () {
+    });
+
+});
+
+app.listen(port, "192.168.31.26", () => console.log(`listening on port ${port}`));
